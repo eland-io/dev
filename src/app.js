088a14436cb5c527c18fc5154b2af77525df4c7d
Soli Tsaagane <51553394+soli-rsa@users.noreply.github.com>
2022-03-30 20:10:53 +0200

    structure & markup refactor

--- src/app.js
+++ src/app.js
@@ -1,13 +1,3 @@
 export default {
-    name : 'Eland.io',
-    
-    data : {
-        async contact(){
-           const res = await fetch('./data.json')
-           const contact = await res.json()
-
-           console.log(contact);
-           return contact
-        }
-    },
+    name : 'Eland.io'   
 }
